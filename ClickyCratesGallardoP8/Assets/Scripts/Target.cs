cbac022234e332363bfbb7d6ad7d2e8911393b5e
slickw <723012@student.dusd.net>
2025-05-20 15:26:04 -0700

    Made my good objects go up
    Worked on assignment

--- ClickyCratesGallardoP8/Assets/Scripts/Target.cs
+++ ClickyCratesGallardoP8/Assets/Scripts/Target.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+       
     // Start is called before the first frame update
     void Start()
     {
@@ -12,10 +18,16 @@ public class Target : MonoBehaviour
         targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
         targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10,10),
             Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), 6);
 
         
+ 
 
-        
+
+
+
+
+
     }
 
     // Update is called once per frame
